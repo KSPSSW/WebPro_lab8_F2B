38391d113870b7c7cbdcbb5347643c549cab44aa
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2025-02-17 00:23:55 +0700

    fix super premium bug

--- PPPPPPP22222/term2/webPro/lab8/frontend/src/stores/userStore.ts
+++ PPPPPPP22222/term2/webPro/lab8/frontend/src/stores/userStore.ts
@@ -65,60 +65,62 @@ export const useUserStore = defineStore('user', () => {
     }
   }
   function getUserByEmail(login: string): User | undefined {
-    async function getUsers() {
-      try {
-        Loading.show()
-        const res = await api.get('/users')
-        users.value = res.data
-        console.log(users.value)
-      } catch (err) {
-        console.error(err)
-        $q.notify({
-          color: 'negative',
-          position: 'top-right',
-          message: 'Users were not loaded',
-          icon: 'report_problem',
-        })
-      } finally {
-        console.log('finally')
-        Loading.hide()
-        $q.notify({
-          color: 'positive',
-          position: 'top',
-          message: 'Users were loaded',
-          icon: 'check_circle',
-        })
-      }
-    }
-    async function getUsersbyDelete() {
-      try {
-        Loading.show()
-        const res = await api.get('/users')
-        users.value = res.data
-        console.log(users.value)
-      } catch (err) {
-        console.error(err)
-        $q.notify({
-          color: 'negative',
-          position: 'top-right',
-          message: 'Users were not loaded',
-          icon: 'report_problem',
-        })
-      } finally {
-        console.log('finally')
-        Loading.hide()
-        $q.notify({
-          color: 'positive',
-          position: 'top',
-          message: 'Users were deleted',
-          icon: 'check_circle',
-        })
-      }
-    }
-
-    return { users, addUser, delUser, updateUser, getUserByEmail, getUsers, getUsersbyDelete }
+    return users.value.find((item) => item.login === login)
   }
-  return { users, addUser, delUser, updateUser, getUserByEmail }
+
+  async function getUsers() {
+    try {
+      Loading.show()
+      const res = await api.get('/users')
+      users.value = res.data
+      console.log(users.value)
+    } catch (err) {
+      console.error(err)
+      $q.notify({
+        color: 'negative',
+        position: 'top-right',
+        message: 'Users were not loaded',
+        icon: 'report_problem',
+      })
+    } finally {
+      console.log('finally')
+      Loading.hide()
+      $q.notify({
+        color: 'positive',
+        position: 'top',
+        message: 'Users were loaded',
+        icon: 'check_circle',
+      })
+    }
+  }
+
+  async function getUsersbyDelete() {
+    try {
+      Loading.show()
+      const res = await api.get('/users')
+      users.value = res.data
+      console.log(users.value)
+    } catch (err) {
+      console.error(err)
+      $q.notify({
+        color: 'negative',
+        position: 'top-right',
+        message: 'Users were not loaded',
+        icon: 'report_problem',
+      })
+    } finally {
+      console.log('finally')
+      Loading.hide()
+      $q.notify({
+        color: 'positive',
+        position: 'top',
+        message: 'Users were deleted',
+        icon: 'check_circle',
+      })
+    }
+  }
+
+  return { users, addUser, delUser, updateUser, getUserByEmail, getUsers, getUsersbyDelete }
 })
 
 if (import.meta.hot) {
