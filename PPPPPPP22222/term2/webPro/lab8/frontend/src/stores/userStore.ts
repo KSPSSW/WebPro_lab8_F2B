b97a12458aaebfbd6b99d8a5fff466b5c97a8a9d
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2025-02-17 00:19:31 +0700

    add loading addUser DeleteUser and UpdateUser

--- PPPPPPP22222/term2/webPro/lab8/frontend/src/stores/userStore.ts
+++ PPPPPPP22222/term2/webPro/lab8/frontend/src/stores/userStore.ts
@@ -1,26 +1,68 @@
 import { defineStore, acceptHMRUpdate } from 'pinia'
-import { Loading } from 'quasar'
+import { Loading, useQuasar } from 'quasar'
 import { api } from 'src/boot/axios'
 import { type User } from 'src/models'
 import { ref } from 'vue'
 
 export const useUserStore = defineStore('user', () => {
   const users = ref<User[]>([])
-  let lastUserId = 3
-  function addUser(u: User) {
-    users.value.push({ ...u, id: lastUserId++ })
+  const $q = useQuasar()
+  async function addUser(u: User) {
+    try {
+      Loading.show()
+      const res = await api.post('/users', u)
+      console.log(res.data)
+      await getUsers()
+    } catch (err) {
+      console.error(err)
+      $q.notify({
+        color: 'negative',
+        position: 'top-right',
+        message: 'User was not added',
+        icon: 'report_problem',
+      })
+    } finally {
+      console.log('finally')
+      Loading.hide()
+    }
   }
-  function delUser(u: User) {
-    const index = users.value.findIndex((item) => {
-      return u.id === item.id
-    })
-    users.value.splice(index, 1)
+  async function delUser(u: User) {
+    try {
+      Loading.show()
+      const res = await api.delete('/users/' + u.id)
+      console.log(res.data)
+      await getUsersbyDelete()
+    } catch (err) {
+      console.error(err)
+      $q.notify({
+        color: 'negative',
+        position: 'top-right',
+        message: 'User was not deleted',
+        icon: 'report_problem',
+      })
+    } finally {
+      console.log('finally')
+      Loading.hide()
+    }
   }
-  function updateUser(u: User) {
-    const index = users.value.findIndex((item) => {
-      return u.id === item.id
-    })
-    users.value[index] = { ...u }
+  async function updateUser(u: User) {
+    try {
+      Loading.show()
+      const res = await api.patch('/users/' + u.id, u)
+      console.log(res.data)
+      await getUsers()
+    } catch (err) {
+      console.error(err)
+      $q.notify({
+        color: 'negative',
+        position: 'top-right',
+        message: 'User was not added or updated failed',
+        icon: 'report_problem',
+      })
+    } finally {
+      console.log('finally')
+      Loading.hide()
+    }
   }
   function getUserByEmail(login: string): User | undefined {
     async function getUsers() {
@@ -31,12 +73,50 @@ export const useUserStore = defineStore('user', () => {
         console.log(users.value)
       } catch (err) {
         console.error(err)
+        $q.notify({
+          color: 'negative',
+          position: 'top-right',
+          message: 'Users were not loaded',
+          icon: 'report_problem',
+        })
       } finally {
         console.log('finally')
         Loading.hide()
+        $q.notify({
+          color: 'positive',
+          position: 'top',
+          message: 'Users were loaded',
+          icon: 'check_circle',
+        })
       }
     }
-    return { users, addUser, delUser, updateUser, getUserByEmail, getUsers }
+    async function getUsersbyDelete() {
+      try {
+        Loading.show()
+        const res = await api.get('/users')
+        users.value = res.data
+        console.log(users.value)
+      } catch (err) {
+        console.error(err)
+        $q.notify({
+          color: 'negative',
+          position: 'top-right',
+          message: 'Users were not loaded',
+          icon: 'report_problem',
+        })
+      } finally {
+        console.log('finally')
+        Loading.hide()
+        $q.notify({
+          color: 'positive',
+          position: 'top',
+          message: 'Users were deleted',
+          icon: 'check_circle',
+        })
+      }
+    }
+
+    return { users, addUser, delUser, updateUser, getUserByEmail, getUsers, getUsersbyDelete }
   }
   return { users, addUser, delUser, updateUser, getUserByEmail }
 })
