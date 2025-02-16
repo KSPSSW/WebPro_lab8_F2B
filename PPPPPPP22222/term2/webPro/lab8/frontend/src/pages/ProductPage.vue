d4624c2a28ce106683ab4a0fab094bfc3068994e
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2025-02-17 00:20:35 +0700

    add ProductPage

---
+++ PPPPPPP22222/term2/webPro/lab8/frontend/src/pages/ProductPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page padding>
+    <h1>Product Page</h1>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+// import { useProductStore } from 'src/stores/productStore'
+// const productStore = useProductStore()
+</script>
