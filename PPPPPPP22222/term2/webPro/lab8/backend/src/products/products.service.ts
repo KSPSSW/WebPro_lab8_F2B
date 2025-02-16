97f932082e6efcf9b85ff2b44049dfebf026d2bb
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2025-02-17 01:40:27 +0700

    add PROD fixfixfixfixfixfix

--- PPPPPPP22222/term2/webPro/lab8/backend/src/products/products.service.ts
+++ PPPPPPP22222/term2/webPro/lab8/backend/src/products/products.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-
+import { Product } from './entities/product.entity';
 @Injectable()
 export class ProductsService {
+  lastID: number = 2;
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 100,
+      size: 'small',
+      image: 'image1',
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 200,
+      size: 'medium',
+      image: 'image2',
+    },
+  ];
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const newProduct: Product = {
+      ...createProductDto,
+      id: ++this.lastID,
+    };
+    this.products.push(newProduct);
+    return newProduct;
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.products;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      throw new NotFoundException('Product not found');
+    }
+    return this.products[index];
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      throw new NotFoundException('Product not found');
+    }
+    this.products[index] = { ...this.products[index], ...updateProductDto };
+    return this.products[index];
   }
 
   remove(id: number) {
-    return `This action removes a #${id} product`;
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      throw new NotFoundException('Product not found');
+    }
+    const deletedProduct = this.products[index];
+    this.products.splice(index, 1);
+    return deletedProduct;
   }
 }
+
